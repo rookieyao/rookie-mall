fb3d6038a898857ea5097681ab932516a8fc73a4
rookie <yaoqi@wanhengtech.com>
2020-05-27 09:55:00 +0800

    新增基于nacos的mall-config配置服务

---
+++ mall-config/src/main/java/com/rookie/config/MallConfigApplication.java
@@ -0,0 +1,13 @@
+package com.rookie.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallConfigApplication.class, args);
+    }
+
+}
---
+++ mall-config/src/main/java/com/rookie/config/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.rookie.config.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author rookie
+ * @Date 2020/5/27 9:30
+ * @Description
+ **/
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
