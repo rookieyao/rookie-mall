331189280469af7214be3f9c19b675e17d458daa
rookie <yaoqi@wanhengtech.com>
2020-05-29 14:53:06 +0800

    新增rookie-mall-gateway 新增prometheus监控依赖

--- mall-gateway/src/main/java/com/rookie/gateway/MallGatewayApplication.java
+++ mall-gateway/src/main/java/com/rookie/gateway/MallGatewayApplication.java
@@ -1,15 +1,27 @@
 package com.rookie.gateway;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MallGatewayApplication {
 
+    @Value("${spring.application.name}")
+    private  String application;
+
     public static void main(String[] args) {
         SpringApplication.run(MallGatewayApplication.class, args);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer() {
+        return (registry) -> registry.config().commonTags("application", application);
     }
 
 }
